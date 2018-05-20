403ce15239630ce01b33cc882f441bc494d84a77
jairo <jairo.goncalves90@gmail.com>
2018-05-20 15:34:44 -0300

    Aula 5-2

--- src/componentes/Foto.js
+++ src/componentes/Foto.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router';
-
+import Pubsub from 'pubsub-js';
 
 class FotoAtualizacoes extends Component {
 
@@ -26,8 +26,9 @@ class FotoAtualizacoes extends Component {
               throw new Error("não foi possível realizar o like da foto")
           }
         })
-        .then(like => {
-          this.setState({likeada : !this.state.likeada})          
+        .then(liker => {
+          this.setState({likeada : !this.state.likeada})  
+          Pubsub.publish('atualiza-liker',{fotoId:this.props.foto.id,liker})        
         })
     }
 
@@ -65,14 +66,35 @@ class FotoHeader extends Component {
 }
 
 class FotoInfo extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {likers : this.props.foto.likers};
+  }
+
+  componentWillMount(){
+    Pubsub.subscribe('atualiza-liker',(topico, infoLiker) => {
+      if(this.props.foto.id === infoLiker.fotoId){
+        const possivelLiker = this.state.likers.find(liker => liker.login === infoLiker.liker.login)
+        if(possivelLiker === undefined){
+          const novosLikers = this.state.likers.concat(infoLiker.liker);
+          this.setState({likers:novosLikers});
+        }else{
+          const novosLikers = this.state.likers.filter(liker => liker.login !== infoLiker.liker.login)
+          this.setState({likers:novosLikers})
+        }
+      }
+    });
+  }
+  
   render(){
     const { foto } = this.props
     return(
       <div className="foto-in fo">
         <div className="foto-info-likes">
           {
-            foto.likers.map(liker => {
-              return (<Link key={liker.login} href={`/timeline/${liker.login}`}>{liker.login}</Link>)
+             this.state.likers.map(liker => {
+              return(<a key={liker.login} href="#">{liker.login},</a>)
             })
           }
           curtiram
